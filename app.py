cf1ade35e5d5a47236652998fca37e3473ad283c
Gabriel Martins <gcarvalhomartins23@gmail.com>
2024-04-01 13:43:20 -0400

    clean code

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask,jsonify,request
 from construct_csv import tratando_Csv
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 corpo_csv = {
     "cabecalho": ["name","mago","mago1"],
     "linhas": ["gabrie","mago2","mago3"]
 }
 
-@app.route('/teste',methods=['GET'])
+@APP.route('/teste',methods=['GET'])
 def hello_word():
     return jsonify(corpo_csv)
 
-@app.route('/ccsv',methods=['POST'])
+@APP.route('/ccsv',methods=['POST'])
 def create_csv():
     novo_csv = request.get_json()
     novo_csv = tratando_Csv(novo_csv)
     return novo_csv
 
-app.run(host='localhost', port=3030, debug=True)
+APP.run(host='localhost', port=3030, debug=True)
