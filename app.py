357a46df18bc61e44228501e27c618537334e4fb
Gabriel Martins <gcarvalhomartins23@gmail.com>
2024-04-01 14:31:33 -0400

    ajeitando create_body

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,jsonify,request
-from construct_csv import tratando_Csv
+from construct_csv import tratando_Csv, create_body_csv
 
 APP = Flask(__name__)
 
--- construct_csv.py
+++ construct_csv.py
@@ -1,16 +1,22 @@
 import csv
 
+teste_json = {
+    "cabecalho" : ["valor","valor2","valor3"]
+}
+
 def tratando_Csv(receive_json):
     receive_json.keys()
-    receive_json['cabecalho']
-    receive_json = create_body_csv(receive_json)
+    receive_json = receive_json['cabecalho']
     return receive_json
 
+
 def create_body_csv(heade_csf):
+    heade_csf = tratando_Csv(heade_csf)
     with open('testando.csv', mode='w', newline = '') as csv_file:
-        cabecalho = [heade_csf]
+        cabecalho = heade_csf
 
         writer = csv.DictWriter(csv_file,fieldnames=cabecalho)
         writer.writeheader()
         writer.writerow({"Mago1":"Oi","Mago2":"Sou o Mago","Mago3":"hehehehe"})
 
+create_body_csv(teste_json)
